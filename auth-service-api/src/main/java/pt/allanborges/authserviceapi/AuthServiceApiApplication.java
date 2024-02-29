0c4ca53cc10ce688d6d91f0dd18133cb4a48103c
Allan Borges <91190584+allansingi@users.noreply.github.com>
2024-02-29 11:16:32 +0000

    chore: starter auth-service-api

---
+++ auth-service-api/src/main/java/pt/allanborges/authserviceapi/AuthServiceApiApplication.java
@@ -0,0 +1,13 @@
+package pt.allanborges.authserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApiApplication.class, args);
+	}
+
+}
