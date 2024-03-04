6abd27a40d6c2bbe558ca87c6af9871873f3c7d2
Allan Borges <91190584+allansingi@users.noreply.github.com>
2024-03-04 10:51:02 +0000

    feat: add authentication impl

---
+++ auth-service-api/src/main/java/pt/allanborges/authserviceapi/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package pt.allanborges.authserviceapi.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pt.allanborges.authserviceapi.repositories.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(final String email) throws UsernameNotFoundException {
+        final var entity = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+
+        return null;
+    }
+
+}
