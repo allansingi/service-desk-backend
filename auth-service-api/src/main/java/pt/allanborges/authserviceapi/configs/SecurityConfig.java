5ebcc21f315cad495899f22e2bd28e8079d0af7e
Allan Borges <91190584+allansingi@users.noreply.github.com>
2024-03-04 09:53:51 +0000

    feat: security configuration

--- auth-service-api/src/main/java/pt/allanborges/authserviceapi/configs/SecurityConfig.java
+++ auth-service-api/src/main/java/pt/allanborges/authserviceapi/configs/SecurityConfig.java
@@ -1,9 +1,10 @@
-package configs;
+package pt.allanborges.authserviceapi.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -14,17 +15,21 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf().disable()
-                .authorizeHttpRequests(http ->
-                        http.requestMatchers(
+                .authorizeHttpRequests(http -> http
+                        .requestMatchers(
                                 new AntPathRequestMatcher("/swagger-ui/**"),
-                                new AntPathRequestMatcher("/swagger-ui.html"),
                                 new AntPathRequestMatcher("/v3/api-docs/**"),
-                                new AntPathRequestMatcher("/api/auth/login/**")
+                                new AntPathRequestMatcher("/api/auth/**")
                         ).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
