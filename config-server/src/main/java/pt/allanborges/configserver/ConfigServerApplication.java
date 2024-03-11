eeedfa6f81d6497d01ae27e27aa8c1cbee224d81
Allan Borges <91190584+allansingi@users.noreply.github.com>
2024-03-11 11:57:53 +0000

    chore: start config server

---
+++ config-server/src/main/java/pt/allanborges/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pt.allanborges.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
